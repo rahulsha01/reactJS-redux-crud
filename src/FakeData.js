57d55101ba4c88eb1b600005a3ab07ca45055fa2
rahulsha01 <make.rahulsharma@gmail.com>
2022-09-07 09:47:29 +0530

    redux implemented

---
+++ src/FakeData.js
@@ -0,0 +1,37 @@
+export const UsersData = [
+  {
+    id: 1,
+    name: "Rahul Sharma",
+    username: "rahul.sharma",
+  },
+  {
+    id: 2,
+    name: "Tech House",
+    username: "Tech",
+  },
+  {
+    id: 3,
+    name: "John Lukas",
+    username: "lukas",
+  },
+  {
+    id: 4,
+    name: "Wright brother",
+    username: "brother",
+  },
+  {
+    id: 5,
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+  },
+  {
+    id: 6,
+    name: "Jake Runolfsdottir V",
+    username: "Leopoldo_Corkery",
+  },
+  {
+    id: 7,
+    name: "Mrs. Dennis Schulist",
+    username: "Karley_Dach",
+  }
+];
